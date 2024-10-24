58277407c4e4f82b23fd46200bd434b73b220347
Bro-dep <153253648+Bro-dep@users.noreply.github.com>
2024-10-23 19:59:07 -0700

    stuff

--- Godot Files/team-terror/Scripts/BellHopBob/bell_hop_bob.gd
+++ Godot Files/team-terror/Scripts/BellHopBob/bell_hop_bob.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 1.0
+var SPEED = 2.0
 
 var player = null
 @export var playerNode : CharacterBody3D
@@ -11,11 +11,13 @@ var player = null
 @export var wanderTimer : Timer
 @export var idleTimer : Timer
 @export var sight : RayCast3D
+@export var ani : AnimationPlayer
 
 @onready var testingTimer = $"Testing Timer"
 var seen : bool
 var idleTimerStarted : bool
 var stalkingTimerStarted : bool
+var seenTimerStarted : bool
 var randomPos
 var wandertime
 #States
@@ -38,6 +40,7 @@ func _process(delta):
 	if sight.get_collider() == playerNode:
 		seen = true
 	else:
+		
 		seen = false
 	#Change state
 	if seen == true:
@@ -45,6 +48,8 @@ func _process(delta):
 	#States
 	match entity_state:
 		0: #Idle
+			SPEED = 1.0
+			ani.play('BobAnimations/Walk')
 			#Stop moving for a little
 			if idleTimerStarted == false:
 				idleTimer.start()
@@ -53,18 +58,23 @@ func _process(delta):
 			
 			pass
 		1: #Chase - When seen/Heard
+			SPEED = 3
+			ani.play('BobAnimations/Run')
 			#Get players location
 			setTarget(playerNode.global_transform.origin)
 			#move to location
 			look_at(playerNode.global_transform.origin)
 			moveTo()
 			#If player is out of los
-			if seen == false:
+			if seen == false and seenTimerStarted == false:
 				seenTimer.start()
+				seenTimerStarted = true
 				#Wander
 			
 			
 		2: #Stalking - Get closer to player
+			SPEED = 1.0
+			ani.play('BobAnimations/Walk')
 			#Get Players Pos
 			setTarget(playerNode.global_transform.origin)
 			#Move To
@@ -78,6 +88,8 @@ func _process(delta):
 			
 			
 		3: #Wandering
+			SPEED = 1.0
+			ani.play('BobAnimations/Walk')
 			#Find random Pos
 			if(abs(randomPos.x - global_position.x) <= 10 and abs(randomPos.z - global_position.z) <= 10) or wanderTimer.time_left <=0:
 				randomPos = setRandomPos()
@@ -94,7 +106,7 @@ func _process(delta):
 
 #Functions
 func lookAt(target):
-	look_at(Vector3(target.x,2.6,target.z))
+	look_at(Vector3(target.x,3.6,target.z))
 
 func setTarget(target):
 	nav_agent.set_target_position(target)
@@ -124,6 +136,7 @@ func _on_idle_timer_timeout() -> void:
 
 func _on_seen_timer_timeout() -> void:
 	#switch to wander
+	seenTimerStarted = false
 	entity_state = 3
 
 func _on_stalking_timer_timeout() -> void:
@@ -146,8 +159,12 @@ func _on_testing_timer_timeout() -> void:
 	elif entity_state == 3:
 		print_debug("wandering")
 	#print(idleTimer.time_left)
+	if seen == false:
+		print('not seen')
+	print_debug(seenTimer.time_left)
 	print_debug(nav_agent.distance_to_target())
 
+#signals
 func _on_hearing_body_entered(body: Node3D) -> void:
 	#set to chase
 	if body.name == 'Player':
@@ -167,21 +184,3 @@ func _on_kill_body_entered(body: Node3D) -> void:
 
 func _on_kill_body_exited(body: Node3D) -> void:
 	pass # Replace with function body.
-
-'
-func look_at(target):
-	pass
-
-func chase():
-	look_at(player.position)
-	nav_agent.target_position = player.global_position
-
-func wandering():
-	look_at(global_transform.origin + velocity)
-	hasSeen = false
-	nav_agent.target_position = randomPos
-	if(abs(randomPos.x - global_position.x) <= 5 and abs(randomPos.z - global_position.z) <= 5) or wanderTimer <=0:
-		randomPos = Vector3(randf_range(player.global_position.x-40, player.global_position.x+40), position.y,
-		 randf_range(player.global_position.z-40, player.global_position.z+40))
-		wanderTimer = 60.0
-'
